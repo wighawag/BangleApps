ee22fa15f46655e71b0e0643405be6bef739543b
wighawag <wighawag@gmail.com>
2022-06-07 16:31:55 +0100

    configurable

--- apps/presentation_timer/app.js
+++ apps/presentation_timer/app.js
@@ -1,11 +1,27 @@
-g.setFontAlign(0,0); // center font
-g.setFont("6x8",8); // bitmap font, 8x magnified
-
-var MINUTES = 1; // 60
+var INITIAL_TOTAL = 15;
+var totalList = [INITIAL_TOTAL, 20, 25, 30, 5, 10];
+var totalIndex = 0;
+var total = INITIAL_TOTAL;
+var MINUTES = 60;
 var steps = [3*MINUTES, 6*MINUTES, 9*MINUTES, 12*MINUTES, 13*MINUTES, 14*MINUTES]
 var currentStep = 0;
 var counter;
 var counterInterval;
+
+// taken from a_speech_timer (https://github.com/espruino/BangleApps/tree/master/apps/a_speech_timer)
+function timeToString(duration) {
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
 
 function outOfTime() {
   if (counterInterval) return;
@@ -22,7 +38,7 @@ function countDown() {
   counter++;
   
 
-  var last = steps[steps.length - 1];
+  var last = Math.floor((total * 60));
   var count = 0;
   if (counter < last) {
     count = last - counter;
@@ -31,17 +47,14 @@ function countDown() {
   
 
   g.clear();
-  g.setFontAlign(0,0); // center font
-  g.setFont("Vector",80); // vector font, 80px  
-  // draw the current counter value
-  g.drawString(count,120,120);
-  // optional - this keeps the watch LCD lit up
+  g.setFontAlign(0,1);
+  g.setFont("Vector",40); 
+  g.drawString(timeToString(count),120,120);
   Bangle.setLCDPower(1);
 
   if (currentStep < steps.length) {
-    if (counter >= steps[currentStep]) {
-      console.log({counter:counter,count:count});
-      E.showMessage("" + steps[currentStep], "PROGRESS");
+    if (counter >= Math.floor(steps[currentStep] * total / INITIAL_TOTAL)) {
+      E.showMessage("" + timeToString(steps[currentStep]), "PROGRESS");
       currentStep++;
       Bangle.buzz();
       Bangle.beep(200, 4000)
@@ -68,4 +81,26 @@ function startTimer() {
     counterInterval = setInterval(countDown, 1000);
 }
 
-startTimer();
+
+
+setWatch(startTimer, (process.env.HWVERSION==2) ? BTN1 : BTN2)
+
+function drawTotal() {
+  g.clear();
+  g.setFontAlign(0,1);
+  g.setFont("Vector",40); 
+  g.drawString(timeToString(total * 60),120,120);
+  Bangle.setLCDPower(1);
+}
+
+Bangle.on('touch', function(zone,e) {
+  console.log("touched")
+  totalIndex++;
+  if (totalIndex >= totalList.length) {
+    totalIndex = 0;
+  }
+  total = totalList[totalIndex];
+  drawTotal();
+});
+
+drawTotal();
